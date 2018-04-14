24d242407cc2de54b71d46c708d83581b74ababc
Rajasekar <rajasekarans.rk@gmail.com>
2018-04-14 15:42:07 +0530

    added header sidebar and dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
